fb6714d8ccb73f5c5f8bc4f51f58b8322c333272
Matthew Carter <phuded@gmail.com>
2011-09-05 23:01:27 +0100

    changed for new apache server config
    Signed-off-by: Matthew Carter <phuded@gmail.com>

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-$link = mysql_connect('higherorlower.db.7613256.hostedresource.com', 'higherorlower', 'Thorndon32!');
+//$link = mysql_connect('higherorlower.db.7613256.hostedresource.com', 'higherorlower', 'Thorndon32!');
+$link = mysql_connect('184.168.194.11', 'higherorlower', 'Thorndon32!');
+
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
