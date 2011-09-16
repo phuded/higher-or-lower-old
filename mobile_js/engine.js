31e1a499619fab7dee91ff5e70a54cf822b909df
Matthew Carter <phuded@gmail.com>
2011-09-16 11:12:57 +0100

    Images preloaded

--- mobile_js/engine.js
+++ mobile_js/engine.js
@@ -1,6 +1,6 @@
 
 $.prepareGame = function(){
-		
+
 	//Get player list
 	$.getPlayerList();
 	
@@ -94,15 +94,15 @@ $.startGame = function(){
 		
 		//Hide any current card
 		$("#cardDisplay").css("background-color","");
-		$("#card").hide();
 		
 		//Close Dialogue
-		$.closeForm();
-		//Display card
-		$.displayCard(currentCard);
-
-		//Reset bet slider
-		$("#currentNumFingers").val(0).slider("refresh");
+		$.preLoadImages('images/allcards.png',function() {
+			$.closeForm();
+			//Display card
+			$.displayCard(currentCard);
+			//Reset bet slider
+			$("#currentNumFingers").val(0).slider("refresh");
+		});
 	}
 };
 
@@ -334,6 +334,39 @@ $.getCardCoords = function(card){
 
 	return x + "px " + y;
 };
+ 
+
+$.preLoadImages = function(imageList,callback) {
+		var pic = [], i, total, loaded = 0;
+		if (typeof imageList != 'undefined') {
+			if ($.isArray(imageList)) {
+				total = imageList.length; // used later
+					for (i=0; i < total; i++) {
+						pic[i] = new Image();
+						pic[i].onload = function() {
+							loaded++; // should never hit a race condition due to JS's non-threaded nature
+							if (loaded == total) {
+								if ($.isFunction(callback)) {
+									callback();
+								}
+							}
+						};
+						pic[i].src = imageList[i];
+					}
+			}
+			else {
+				pic[0] = new Image();
+				pic[0].onload = function() {
+					if ($.isFunction(callback)) {
+						callback();
+					}
+				}
+				pic[0].src = imageList;
+			}
+		}
+		pic = undefined;
+};
+
 
 //Game variables
 var players = new Array();
