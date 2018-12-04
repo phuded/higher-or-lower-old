924d03fcdb102ca0115c99bafe7d4367d8f61766
Matt Carter <mcarter@ooyala.com>
2018-12-04 13:24:22 +0000

    Updated DB

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //$link = mysql_connect('higherorlower.db.7613256.hostedresource.com', 'higherorlower', 'Thorndon32!');
-$link = mysql_connect('184.168.194.11', 'higherorlower', 'Thorndon32!');
+$link = mysql_connect('higherorlower.cklqq8fsjhst.eu-west-1.rds.amazonaws.com', 'higherorlower', 'Thorndon32!');
 
 if (!$link) {
     die('Could not connect: ' . mysql_error());
